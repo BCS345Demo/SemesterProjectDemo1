3aaaeb09655b88eb19c3d4b80408c2612ef44a5d
Malrajab <moaath78@gmail.com>
2021-04-24 15:15:47 -0400

    Add launch to the main to start the app.

--- src/semesterproj1/SemesterProj1.java
+++ src/semesterproj1/SemesterProj1.java
@@ -21,7 +21,7 @@ public class SemesterProj1 extends Application{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
 
     @Override
