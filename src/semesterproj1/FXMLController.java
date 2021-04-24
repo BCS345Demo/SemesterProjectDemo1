e6207482bca47e8d5d8576a296a4dd2410ea1509
Malrajab <moaath78@gmail.com>
2021-04-24 14:54:22 -0400

    Add the FXML and the Controller Class

---
+++ src/semesterproj1/FXMLController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semesterproj1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author moaat
+ */
+public class FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
