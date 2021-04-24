97a26216377a2827535a8d29d767ace781e80574
Malrajab <moaath78@gmail.com>
2021-04-24 15:15:13 -0400

    Complete the driver class

--- src/semesterproj1/SemesterProj1.java
+++ src/semesterproj1/SemesterProj1.java
@@ -5,17 +5,30 @@
  */
 package semesterproj1;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author MoaathAlrajab
  */
-public class SemesterProj1 {
+public class SemesterProj1 extends Application{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent prt= FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        stage.setScene(new Scene(prt));
+        stage.show();
     }
     
 }
